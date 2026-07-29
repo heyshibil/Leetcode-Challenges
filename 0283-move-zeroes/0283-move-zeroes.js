8667b4d845404afb4957386d843cf1e8d86f1248
Shibil Mohammed <124491086+heyshibil@users.noreply.github.com>
2026-07-29 11:57:53 +0530

    Time: 0 ms (100%), Space: 60.9 MB (19.11%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -2,18 +2,15 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function (nums) {
-    let lastNonZeroFoundAt = 0;
+var moveZeroes = function(nums) {
+    let j = 0;
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] !== 0) {
-            nums[lastNonZeroFoundAt] = nums[i];
-            lastNonZeroFoundAt++;
+            let temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            j++;
         }
     }
-
-    for (let i = lastNonZeroFoundAt; i < nums.length; i++) {
-        nums[i] = 0;
-    }
-
-    return nums;
 };
+
